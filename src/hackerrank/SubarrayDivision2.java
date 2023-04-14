8bf67ed8dce2785ae991b8b44e0635b1b0cf9d0e
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-04-14 17:57:52 -0300

    Subarray Divison WIP

--- src/hackerrank/SubarrayDivision2.java
+++ src/hackerrank/SubarrayDivision2.java
@@ -8,56 +8,65 @@ import java.util.stream.Collectors;
 
 public class SubarrayDivision2 {
     public static int birthday(List<Integer> s, int d, int m) {
-        Integer[] valores = new Integer[m];
-        Arrays.fill(valores, 0);
-
-        s = s.stream().distinct().collect(Collectors.toList());
+        Integer[] posicoes = new Integer[m];
+        Arrays.fill(posicoes, 0);
 
         List<List<Integer>> combinacoes = new ArrayList<>();
 
-        while(true) {
-            if(valorFinal(valores, s.size())) break;
-
+        do {
             for(int i = m - 1; i >= 0; i--) {
-                if(valores[i] < s.size()) {
-                    valores[i] += 1;
+                if(posicoes[i] < s.size() - 1) {
+                    posicoes[i] += 1;
                     break;
-                } else if(valores[i] == s.size()) {
-                    valores[i] = 0;
-                    if(valores[i - 1] != s.size()) {
-                        valores[i - 1] += 1;
+                } else if(posicoes[i] == s.size() - 1) {
+                    posicoes[i] = 0;
+                    try {
+                        if(posicoes[i - 1] != s.size() - 1) {
+                            posicoes[i - 1] += 1;
+                            break;
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
                         break;
                     }
                 }
             }
 
-
-            if(valoresDistintos(valores)) {
-                int soma = Arrays.stream(valores).sum();
-
+            if(valoresDistintos(posicoes)) {
+                Integer[] valores = new Integer[m];
+                int soma = 0;
+                for(int i = 0; i < posicoes.length; i++) {
+                    valores[i] = s.get(posicoes[i]);
+                    soma += s.get(posicoes[i]);
+                }
+                System.out.println(Arrays.toString(valores));
                 if (soma == d) {
-                    combinacoes.add(Arrays.asList(valores));
+                    combinacoes.add(Arrays.stream(valores).sorted().collect(Collectors.toList()));
                 }
             }
-        }
+        } while (!Arrays.stream(posicoes).allMatch(i -> i == s.size() - 1));
 
-        return combinacoes.size();
+        return (int) combinacoes.stream().distinct().count();
     }
 
     public static boolean valoresDistintos(Integer[] array) {
-        return Arrays.stream(array).distinct().count() == array.length;
-    }
+        Integer valorUnico = array[0];
 
-    public static boolean valorFinal(Integer[] array, int valor) {
-        if(Arrays.stream(array).distinct().count() == 1 && array[0] == valor)
-            return true;
-        else
-            return false;
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] == valorUnico) return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        List<Integer> segmentos = new ArrayList<>(Arrays.asList(1, 2, 1, 3, 2));
+        List<Integer> segmentos = null;
 
+        segmentos = new ArrayList<>(Arrays.asList(4, 5, 4, 2, 4, 5, 2, 3, 2, 1, 1, 5, 4));
+        System.out.println(birthday(segmentos, 15, 4));
+        segmentos = new ArrayList<>(Arrays.asList(1, 2, 1, 3, 2));
         System.out.println(birthday(segmentos, 3, 2));
+        segmentos = new ArrayList<>(Arrays.asList(4));
+        System.out.println(birthday(segmentos, 4, 1));
+
     }
 }

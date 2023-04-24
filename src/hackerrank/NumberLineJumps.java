02508fb8e694355edd65b5ade8ab209de42062a6
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-04-24 17:40:02 -0300

    Number Line Jumps

---
+++ src/hackerrank/NumberLineJumps.java
@@ -0,0 +1,22 @@
+package hackerrank;
+
+public class NumberLineJumps {
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        while(true) {
+            if(x1 == x2)
+                return "YES";
+            if(x1 > x2)
+                break;
+
+            x1 += v1;
+            x2 += v2;
+        }
+
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kangaroo(2, 1, 1, 2));
+        System.out.println(kangaroo(1113, 612, 1331, 610));
+    }
+}
